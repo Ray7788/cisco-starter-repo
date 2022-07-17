9a1769eaa222558906394847a26c29d13f5b10bd
Ray7788 <ray778@foxmail.com>
2022-07-17 15:14:39 +0100

    Task4 almost finished

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,16 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
+
+// Task2
 import Banner from 'react-js-banner';
 
+// Task3
 import Exhibit from './Exhibit';
 import AddressDisplay from './AddressDisplay';
+// Task4
+import PylonConnector from './PylonConnector';
 
 function App() {
   return (
     <div className="App">
       <Banner 
         title={"CISCO Sextant 2022"} 
-        
+        // design a header banner
       />
       
       {/* IPv4 */}
@@ -21,6 +26,11 @@ function App() {
       {/* IPv6 */}
       <Exhibit name="User's IPv6 Address">
           <AddressDisplay url='https://api64.ipify.org?format=json' />
+      </Exhibit>
+
+      {/* Websocket */}
+      <Exhibit name="Pylon Packet Latency">
+            <PylonConnector />
       </Exhibit>
 
       {/* <header className="App-header">
